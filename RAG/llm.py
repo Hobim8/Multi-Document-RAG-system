40c9d642fbf65c723e50912398ecff1c916ffe56
Hobim <hobimfx@outlook.com>
2026-02-07 00:14:27 +0100

    Refactor ask_question function to utilize SYSTEM_PROMPT for dynamic prompts

--- RAG/llm.py
+++ RAG/llm.py
@@ -1,6 +1,7 @@
 import os 
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from prompts import SYSTEM_PROMPT
 
 
 load_dotenv()
@@ -12,17 +13,25 @@ def get_llm():
         temperature = 0.7
     )
 
-# testing llm function
+def ask_question(question: str, context: str = "") -> str:
+    llm = get_llm()
+    prompt = SYSTEM_PROMPT.format(question=question, context=context)
+    response = llm.invoke(prompt)
+    return response.content
+
 
 def test_llm():
-
-    llm = get_llm()
-    response = llm.invoke("what the story behind cleopatra?")
-    print(response.content)
-
-
-# run this if the function works 
+    print("TEST 1: With Context ")
+    context = "Cleopatra was the last pharaoh of Egypt, ruling from 51-30 BC."
+    answer = ask_question("Who was Cleopatra?", context)
+    print(answer)
+    print("\n")
+    
+    print("TEST 2: Without Context")
+    answer = ask_question("What is the story behind Cleopatra?", context="")
+    print(answer)
 
 if __name__ == "__main__":
     test_llm()
+
 
